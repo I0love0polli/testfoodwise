7742ecb586040d8f42df12cd40f2a8f732a9f671
I0love0polli <115264427+I0love0polli@users.noreply.github.com>
2025-06-05 18:37:02 +0200

    test 4
    test 4 spero vada

---
+++ foodwise/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8d81f1bf6d5d019a1ca6206e6bad19acf0d08efd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8d81f1bf6d5d019a1ca6206e6bad19acf0d08efd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
